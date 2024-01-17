18f1aa6a1b406bac3b48c2fcbc311e61ba1f32de
Maria <pomazkinam1997@gmail.com>
2024-01-16 21:02:42 -0700

    Тема 2 ООП: Указать в методах возвращающие типы, типы для параметров, в теле функции также указать типы

--- app/services/general/general.ts
+++ app/services/general/general.ts
@@ -1,21 +1,17 @@
 
 /* Общие методы используются для вставки текста в header   footer*/
 
-/*  -
-    - Указать в методах возвращающие типы, типы для параметров, в теле функции также указать типы
-*/
-
-export function initHeaderTitle(ticketName, selector): void {
-    const headerElement= document.querySelector('header');
-    const targetItem = headerElement.querySelector(selector);
+export function initHeaderTitle(ticketName: string, selector: string): void {
+    const headerElement: HTMLElement = document.querySelector('header');
+    const targetItem: HTMLElement = headerElement.querySelector(selector);
     if (targetItem) {
         targetItem.innerText = ticketName;
     }
 }
 
-export function initFooterTitle(ticketName, selector): void {
-    const headerElement = document.querySelector('footer');
-    const targetItem = headerElement.querySelector(selector);
+export function initFooterTitle(ticketName: string, selector: string): void {
+    const headerElement: HTMLElement = document.querySelector('footer');
+    const targetItem: HTMLElement = headerElement.querySelector(selector);
     if (targetItem) {
         targetItem.innerText = ticketName;
     }
