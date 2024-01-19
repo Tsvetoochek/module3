8132011b4b3b7d6fe3bb49ba30073e8a5917f73d
Maria <pomazkinam1997@gmail.com>
2024-01-18 21:39:47 -0700

    Тема 2 ООП

---
+++ app/services/tickets/ticket.ts
@@ -0,0 +1,60 @@
+import {IVipTicket, TicketType} from "../../models/ticket/ticket";
+import {initTicketElementTemplate} from "../../templates/ticketInfo";
+import {postTicketData} from "@rest/tickets";
+
+let ticketPostInstance;
+
+export function initTicketInfo(ticket: TicketType | IVipTicket) {
+    const targetElement = document.querySelector('.ticket-info');
+
+    const ticketDescription = ticket?.description;
+    const ticketOperator = ticket?.tourOperator;
+    let vipClientType: string;
+    if ("vipStatus" in ticket) {
+        vipClientType = ticket.vipStatus;
+    }
+
+
+    const ticketElemsArr: [string, string, string] = [ticketDescription, ticketOperator, vipClientType];
+    let ticketElemTemplate: string;
+
+    ticketElemsArr.forEach((el, i) => {
+        ticketElemTemplate+= initTicketElementTemplate(el, i);
+    });
+
+    targetElement.innerHTML = ticketElemTemplate;
+
+}
+
+function initUserData() {
+    const userInfo = document.querySelectorAll('.user-info > p');
+    let userInfoObj: Record<string, string>;
+    userInfo.forEach((el) => {
+        const inputDataName = el.getAttribute('data-name');
+        if (inputDataName) {
+            const inputElems = el.querySelector('input');
+            userInfoObj[inputDataName] = inputElems.value;
+        }
+    });
+
+    console.log('userInfoObj',userInfoObj)
+    return userInfoObj;
+}
+
+function initPostData(data) {
+    initUserData();
+    postTicketData(data).then((data) => {
+        if (data.success) {
+
+        }
+    })
+}
+
+export function registerConfirmButton(): void {
+    const targetEl = document.getElementById('accept-order-button');
+    if (targetEl) {
+        targetEl.addEventListener('click', () => {
+            initPostData(ticketPostInstance);
+        });
+    }
+}
